17866357ad626226f05f59fb2a5b7d8076b895ab
KrvdDvrk <johansson_neves_1995@hotmail.com>
2023-10-19 13:20:47 -0300

    version1.2

--- Documents/ProjectsX/hello.bat
+++ Documents/ProjectsX/hello.bat
@@ -14,30 +14,45 @@ Execução:
 	ou
 	./hello.bat
 """
-__version__ = "0.0.1"
+__version__ = "0.1.1" "0.1.2"
 __author__ = "KrvdDvrk"
 __license__ = "Unlicense"
 
+#__version__ = "0.1.1"
+#import os
+#
+#Iniciando aqui o programa
+#
+#current_language = os.getenv("LANG")[:5]
+#
+#msg = "Hello, World!"
+#
+#if current_language == "pt_BR":
+#	msg = "Olá, Mundo!"
+#elif current_language == "it_IT":
+#	msg = "Ciao, Mondo!"
+#elif current_language == "es_ES":
+#    msg = "Hola, Mundo!"
+#elif current_language == "fr_FR":
+#    msg = "Bonjour Monde!"
+#elif current_language == "zh_CN":
+#    msg = "你好世界!"
+#
+#print(msg)
+#print(56 + 4)
+
+__version__ = "0.1.2"
+
 import os
 
-#Iniciando aqui o programa
+current_language = os.getenv("LANG", "en_US")[:5]
 
-current_language = os.getenv("LANG")[:5]
+msg = {
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_ES": "Hola, Mundo!",
+    "fr_FR": "Bonjour Monde!",
+    "zh_CN": "你好世界!",
+}
 
-msg = "Hello, World!"
-
-if current_language == "pt_BR":
-	msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-	msg = "Ciao, Mondo!"
-elif current_language == "es_ES":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde!"
-elif current_language == "zh_CN":
-    msg = "你好世界!"
-
-print(msg)
-print(56 + 4)
-
-
+print(msg[current_language])
