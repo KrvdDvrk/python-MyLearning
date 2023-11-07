0e0fe32cf4fb1575343fc0b348d572aef8c07dcb
KrvdDvrk <johansson_neves_1995@hotmail.com>
2023-11-07 15:55:25 -0300

    Retry com for

--- Xpython_KrvdDvrk/Errors/errors_EAFP.py
+++ Xpython_KrvdDvrk/Errors/errors_EAFP.py
@@ -2,17 +2,31 @@
 
 import os
 import sys
+import time
+import logging
 
-try:
-    names = open("names.txt").readlines()
-    # FileNotFoundError
-except FileNotFoundError as e:
-    print(f"{str(e)}.")
-    sys.exit(1)
-else:
-    print("Sucesso!!!")
-finally:
-    print("Execute isso sempre!")
+log = logging.Logger("errors")
+
+# EAFP - Easy to Ask Forgiveness than permission
+# É mais fácil pedir perdão do que permissão
+
+def try_to_open_a_file(filepath, retry =1) -> list:
+    """Tries to open a file, if error, retries n times"""
+    for attempt in range(1, retry + 1):
+        try:
+            return open(filepath).readlines()
+        except FileNotFoundError as e:
+            log.error("ERRO: %s", e)
+            time.sleep(2)
+        else:
+            print("Sucesso!!!")
+        finally:
+            print("Execute isso sempre!")
+    return []
+
+
+for line in try_to_open_a_file("names.txt", retry=5):
+    print(line)
 
 #OUTROS MODOS DE RESOLVER ERROS:
 #try:
@@ -34,10 +48,4 @@ finally:
 #    raise RuntimeError("Ocorreu um erro")
 #except Exception as e:
 #    print(str(e))
-
-try:
-    print(names[2])
-except:
-    print("[Error] Missing name in the list")
-    sys.exit(1)
 
